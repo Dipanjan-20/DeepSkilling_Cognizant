3dbd83773e11c3c5ea5bfd23a97a048eadf3ce1f
Dipanjan-20 <dipanjan1604@gmail.com>
2025-07-05 22:48:28 +0530

    Add Week3 folder

---
+++ week3/CustomerCommLib/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerCommTests.Tests
+{
+    public interface IMailSender
+    {
+        bool SendMail(string to, string body);
+    }
+
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            return _mailSender.SendMail("customer@example.com", "Hello Customer");
+        }
+    }
+
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender>? _mockMailSender;
+        private CustomerComm? _customerComm;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _mockMailSender
+                .Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            _customerComm = new CustomerComm(_mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue_WhenMailIsSentSuccessfully()
+        {
+            Assert.That(_customerComm, Is.Not.Null); // Replaced Assert.IsNotNull with Assert.That and Is.Not.Null
+
+            var result = _customerComm!.SendMailToCustomer();
+
+            Assert.That(result, Is.Not.Null); // This should work with NUnit
+        }
+    }
+}
---
+++ week3/CustomerCommLib/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCommLib
+{
+    internal class CustomerComm
+    {
+        private IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            return _mailSender.SendMail("dipanjan1604@gmail.com.com", "lets see");
+        }
+    }
+}
